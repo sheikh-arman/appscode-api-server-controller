672fe1625541e5283706c59b3e5beec54da3d5ec
sheikh-arman <arman@appscode.com>
2024-02-21 22:02:36 +0600

    Deployment creation of employee resource
    Signed-off-by: sheikh-arman <arman@appscode.com>

--- pkg/controllers/employee.go
+++ pkg/controllers/employee.go
@@ -1,11 +1,17 @@
 package controllers
 
 import (
+	"context"
 	"fmt"
+	v1alpha1 "github.com/sheikh-arman/controller-appscode-api/pkg/apis/appscode.com/v1alpha1"
 	klient "github.com/sheikh-arman/controller-appscode-api/pkg/client/clientset/versioned"
 	informer "github.com/sheikh-arman/controller-appscode-api/pkg/client/informers/externalversions/appscode.com/v1alpha1"
 	lister "github.com/sheikh-arman/controller-appscode-api/pkg/client/listers/appscode.com/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
 	"log"
@@ -13,6 +19,8 @@ import (
 )
 
 type Controller struct {
+	//clientset for kubernetes resource
+	clientset kubernetes.Interface
 	//klientset for custom resource employee
 	klient klient.Interface
 	// employee synced
@@ -23,8 +31,9 @@ type Controller struct {
 	wordQueue workqueue.RateLimitingInterface
 }
 
-func NewController(klient klient.Interface, employeeInformer informer.EmployeeInformer) *Controller {
+func NewController(clientset kubernetes.Interface, klient klient.Interface, employeeInformer informer.EmployeeInformer) *Controller {
 	c := &Controller{
+		clientset:      clientset,
 		klient:         klient,
 		employeeSynced: employeeInformer.Informer().HasSynced,
 		lister:         employeeInformer.Lister(),
@@ -40,11 +49,11 @@ func NewController(klient klient.Interface, employeeInformer informer.EmployeeIn
 }
 
 func (c *Controller) Run(ch chan struct{}) error {
-	fmt.Println("Check culprit->>>>>>>")
+	//fmt.Println("Check culprit->>>>>>>")
 	if ok := cache.WaitForCacheSync(ch, c.employeeSynced); !ok {
 		log.Println("Cache was not synced")
 	}
-	fmt.Println("Check 1->>>>>>>")
+	//fmt.Println("Check 1->>>>>>>")
 	go wait.Until(c.worker, time.Second, ch)
 	<-ch
 	return nil
@@ -78,8 +87,11 @@ func (c *Controller) processNextItem() bool {
 		return false
 	}
 	log.Printf("\nEmployee resource:\nName:%s\nSpec%s\n", employee.Name, employee.Spec)
+	c.createDeployment(ns, employee)
+	//c.checkClientSet()
 	return true
 }
+func int32Ptr(i int32) *int32 { return &i }
 func (c *Controller) handleAdd(obj interface{}) {
 	log.Println("Resource added")
 	c.wordQueue.Add(obj)
@@ -88,4 +100,65 @@ func (c *Controller) handleAdd(obj interface{}) {
 func (c *Controller) handleDelete(obj interface{}) {
 	log.Println("Resource removed")
 	c.wordQueue.Add(obj)
+}
+
+func (c *Controller) createDeployment(ns string, employee *v1alpha1.Employee) {
+	//create deployment for the employee CR
+	deploymentClientSet := c.clientset.AppsV1().Deployments(ns)
+
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: employee.Name,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: int32Ptr(2),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": employee.Name,
+				},
+			},
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": employee.Name,
+					},
+				},
+				Spec: apiv1.PodSpec{
+					Containers: []apiv1.Container{
+						{
+							Name:  employee.Name,
+							Image: employee.Spec.Image,
+							Ports: []apiv1.ContainerPort{
+								{
+									Name:          "http",
+									Protocol:      apiv1.ProtocolTCP,
+									ContainerPort: 8080,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	log.Printf("Creating Deployemnet for %s resource", employee.Name)
+	result, err := deploymentClientSet.Create(context.TODO(), deployment, metav1.CreateOptions{})
+	if err != nil {
+		log.Printf("Error on creating deployment %s", employee.Name)
+		return
+	}
+	log.Printf("Created deployment %s\n", result.GetObjectMeta().GetName())
+}
+
+func (c *Controller) checkClientSet() {
+	res, err := c.clientset.CoreV1().Pods("appscode").List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	//fmt.Println(clientset)
+	fmt.Println("Pods are:", len(res.Items))
+	for _, e := range res.Items {
+		fmt.Println(e.Name)
+	}
 }
--- pkg/controllers/run-api.go
+++ pkg/controllers/run-api.go
@@ -1,13 +1,10 @@
 package controllers
 
 import (
-	"context"
 	"flag"
-	"fmt"
-	"github.com/sheikh-arman/controller-appscode-api/pkg/apis/appscode.com/v1alpha1"
 	klient "github.com/sheikh-arman/controller-appscode-api/pkg/client/clientset/versioned"
 	informer "github.com/sheikh-arman/controller-appscode-api/pkg/client/informers/externalversions"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 	"log"
@@ -29,36 +26,49 @@ func RunApi() {
 	if err != nil {
 		panic(err)
 	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Printf("My on getting kubernetes clientset %s\n", err.Error())
+		panic(err)
+	}
+	//res, err := clientset.CoreV1().Pods("appscode").List(context.Background(), metav1.ListOptions{})
+	////fmt.Println(clientset)
+	//fmt.Println("Pods are:", len(res.Items))
+	//for _, e := range res.Items {
+	//	fmt.Println(e.Name)
+	//}
+
 	klientset, err := klient.NewForConfig(config)
 	if err != nil {
 		log.Printf("My Error!!!!!!!!!!  %s\n", err.Error())
 		panic(err)
 	}
 
-	emp, err := klientset.AppscodeV1alpha1().Employees("").List(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		log.Println(err)
-	}
-	fmt.Println(len(emp.Items))
-	for _, em := range emp.Items {
-		fmt.Println(em.Name, em.Spec.Image)
-	}
-	Employee := v1alpha1.Employee{}
-	fmt.Println(Employee)
+	//emp, err := klientset.AppscodeV1alpha1().Employees("").List(context.Background(), metav1.ListOptions{})
+	//if err != nil {
+	//	log.Println(err)
+	//}
+	//fmt.Println(len(emp.Items))
+	//for _, em := range emp.Items {
+	//	fmt.Println(em.Name, em.Spec.Image)
+	//}
+	//Employee := v1alpha1.Employee{}
+	//fmt.Println(Employee)
 
 	_ = klientset
+	//start controller
 	ch := make(chan struct{})
-
 	informerFactory := informer.NewSharedInformerFactory(klientset, 20*time.Minute)
-	fmt.Println("Check 11->>>>>>>")
-	c := NewController(klientset, informerFactory.Appscode().V1alpha1().Employees())
-	fmt.Println("Check 12->>>>>>>")
+	//fmt.Println("Check 11->>>>>>>")
+	c := NewController(clientset, klientset, informerFactory.Appscode().V1alpha1().Employees())
+	//fmt.Println("Check 12->>>>>>>")
 	informerFactory.Start(ch)
-	fmt.Println("Check 13->>>>>>>")
+	//fmt.Println("Check 13->>>>>>>")
+	log.Println("Starting controller...")
 	if err = c.Run(ch); err != nil {
 		log.Printf("My Error!!!!!!!!!! %s", err.Error())
 	}
-
+	//end controller
 	//fmt.Println(klientset)
 	//emp, err := klientset.AppscodeV1alpha1().Employees("").List(context.Background(), metav1.ListOptions{})
 	//if err != nil {
